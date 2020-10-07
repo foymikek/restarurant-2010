f6f057089f5541feea72271bdca932a32de9e63f
Michael Foy <foymikek@gmail.com>
2020-10-07 13:12:46 -0600

    Initial commit

---
+++ lib/restaurant.rb
@@ -0,0 +1,12 @@
+class Restaurant
+  attr_reader :opening_time, :name, :dishes
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(hours_of_business)
+    "#{(opening_time.to_i + hours_of_business)}:00"
+  end
+end
