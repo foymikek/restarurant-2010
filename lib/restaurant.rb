eebf405a6a948c00e3c1305aec5ffe55ce6cf40a
Michael Foy <foymikek@gmail.com>
2020-10-07 13:52:07 -0600

    Add passing test_restruant_can_announce_closing_time

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -29,5 +29,17 @@ class Restaurant
       dish.upcase
     end
   end
-  
+
+  def announce_closing_time(hours_of_business)
+    "#{@name} will be closing at #{determine_hour(hours_of_business)}"
+  end
+
+  def determine_hour(hours_of_business)
+    if closing_time(hours_of_business).to_i > 12
+      return "#{closing_time(hours_of_business).to_i - 12}:00PM"
+    else
+      return "#{closing_time(hours_of_business).to_i}:00AM"
+    end
+  end
+
 end
